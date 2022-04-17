2cceba98f811acba5d5a51590e35824553a81c4e
Bowen S. Zhu <bszhu@fas.harvard.edu>
2022-04-17 15:19:24 -0400

    Plot eigenvalues of POD correlation

--- demo/ProperOrthogonalDecomposition.jl
+++ demo/ProperOrthogonalDecomposition.jl
@@ -4,13 +4,15 @@ using Symbolics, LinearAlgebra
 n = 100
 a = randn(n)
 x = exp.(a * t)
-
 tspan = (0.0, 10.0)
 m = 10
 snapshots = Symbolics.value.(mapreduce(tᵢ -> substitute.(x, (Dict(t => tᵢ),)),
                                        hcat,
                                        range(tspan[1], tspan[2], m)))
 covariance = Symmetric(snapshots' * snapshots)
-vals, vecs = eigen(covariance)
+vals, vecs = eigen(covariance; sortby=-)
+using Plots
+plot(vals, markershape=:circle, xticks=1:m, yscale=:log10, legend=false)
+d = 5
 projection = (snapshots * (@. vecs * (1 / √vals)'))'
 reduced_model = simplify.(projection * x)
